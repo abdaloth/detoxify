332798dd5795bc68955e51b05c029e900cd5dbd6
abdaloth <abdullahoalothman@gmail.com>
2021-04-29 23:30:15 -0400

    migrate to tf2 + formatting

--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,18 @@
 from locust import FastHttpUser, task
 import json
 
+
 class APIUser(FastHttpUser):
     """
     Simulated API user for load testing
     """
+
     @task
     def create_post(self):
-        headers = {'content-type': 'application/json','Accept-Encoding':'gzip'}
+        headers = {"content-type": "application/json", "Accept-Encoding": "gzip"}
         self.client.post(
             "/secret_add",
-            data= json.dumps(
-                {
-                    "a": 5,
-                    "b": 6
-                }
-            ),
+            data=json.dumps({"a": 5, "b": 6}),
             headers=headers,
-            name = "Add Numbers"
+            name="Add Numbers",
         )
