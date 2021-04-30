4225f750983011dfd3f1e547e9c72fcf81ed3fb1
abdaloth <abdullahoalothman@gmail.com>
2021-04-29 20:12:05 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request, jsonify
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+
+import numpy as np
+import pickle
+import re
+
+
+
+
+app = Flask(__name__)
+
+LINKS_re = re.compile(r'https?://.+?(\s|$)')
+NONALPHANUMERIC_re = re.compile(r'[^\w ]')
+TOK_PATH = 'serialized/tokenizer.pickle'
+MODEL_PATH = 'serialized/cpu_friendly_model.h5'
+MAXLEN = 200  # more than 99th percentile
+
+LABELS = ['toxicity', 'severe_toxicity', 'identity_attack', 'insult', 'threat']
+
+
+
+print('loading tokenizer..')
+with open(TOK_PATH, 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+print('loading model..')
+model = load_model(MODEL_PATH)
+
+
+print('done')
+
+
+def clean_text(comment):
+    cleaned = re.sub(LINKS_re, ' ', comment)
+    cleaned = re.sub(NONALPHANUMERIC_re, ' ', cleaned)
+    cleaned = re.sub(r' +', ' ', cleaned)  # collapse consequtive spaces
+    return cleaned
+
+
+
+def preprocess(text_list):
+    cleaned_text = [clean_text(text) for text in text_list]
+    processed_input = tokenizer.texts_to_sequences(cleaned_text)
+    processed_input = pad_sequences(processed_input, MAXLEN)
+    return processed_input
+
+
+
+@app.route('/')
+def hello():
+    """simple landing Page."""
+    return render_template('index.html')
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    """
+    """
+    form_inputs = request.form.to_dict()
+    result = int(form_inputs['a']) + int(form_inputs['b'])
+
+    return render_template('index.html', result_text = f'A + B = {result}')
+
+
+@app.route('/secret_add', methods=['POST'])
+def secret_add():
+    data = request.get_json()
+    if data is None:
+        return jsonify({ 'error': 'Missing input' }), 400
+    a = int(data.get('a', 0))
+    b = int(data.get('b', 0))
+    return jsonify({'result':a+b})
+
+
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    data = request.get_json()
+    if data is None:
+        return jsonify({ 'error': 'Missing input' }), 400
+    text = data.get('text', 'N/A')
+    prediction = model.predict(preprocess([text]), batch_size=1, verbose=0)
+    return jsonify({"test":"works"})
+    # return jsonify({label:res for (label, res) in zip(LABELS, prediction)})
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ sentiment_model.py
@@ -0,0 +1,71 @@
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+import argparse
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.metrics import roc_auc_score
+from utils import clean_text
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from tqdm import tqdm
+
+
+
+TOK_PATH = 'model_weights/tokenizer.pickle'
+MODEL_PATH = 'model_weights/model.h5'
+MAXLEN = 200  # more than 99th percentile
+
+LABELS = ['toxicity', 'severe_toxicity', 'identity_attack', 'insult', 'threat']
+
+
+def preprocess(text_list, tokenizer_path=TOK_PATH, maxlen=MAXLEN):
+    print('cleaning input text....')
+    cleaned_text = [clean_text(text) for text in tqdm(text_list)]
+    with open(tokenizer_path, 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    print('processing input ....')
+    processed_input = tokenizer.texts_to_sequences(cleaned_text)
+    processed_input = pad_sequences(processed_input, maxlen)
+    return processed_input
+
+
+def get_predicted_labels(model_input, model_path, batch_size=256):
+    print('loading model....')
+    model = load_model(model_path)
+    print('predicting labels....')
+    y_pred = model.predict(model_input, batch_size=batch_size, verbose=1)
+    return y_pred
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_path', default=None,
+                        help="specify the input filename")
+    parser.add_argument('-o', '--output_path',
+                        default='./predicted_labels.csv', help="specify the output filename")
+    parser.add_argument('--id', default='id', help="name of the id columns")
+    parser.add_argument('--text', default='text',
+                        help="name of the text columns")
+    parser.add_argument('-tok', '--tokenizer_path',
+                        default=TOK_PATH, help="path to tokenizer pickle")
+    parser.add_argument('-model', '--model_path',
+                        default=MODEL_PATH, help="path to saved model")
+    parser.add_argument('-bs', '--batch_size', default=64,
+                        help="prediction batch size", type=int)
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.input_path, usecols=[args.id, args.text])
+    model_input = preprocess(df[args.text], args.tokenizer_path)
+    predictions = get_predicted_labels(model_input, args.model_path, args.batch_size)
+    for i, label in enumerate(LABELS):
+        df[label] = predictions[:,i]
+        pass
+
+    df.to_csv(args.output_path, index=False)
