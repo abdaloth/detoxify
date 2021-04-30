dbc433d82fa5757906a3e9a70b81d9c3198c2708
abdaloth <abdullahoalothman@gmail.com>
2021-04-29 20:55:17 -0400

    set a default graph to avoid keras errors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import warnings
-warnings.filterwarnings('ignore', category=FutureWarning)
-warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 import tensorflow as tf
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from keras.preprocessing.sequence import pad_sequences
@@ -12,39 +14,27 @@ from keras.models import load_model
 import numpy as np
 import pickle
 import re
+import tensorflow as tf
 
-
+graph = tf.get_default_graph()
 
 
 app = Flask(__name__)
 
-LINKS_re = re.compile(r'https?://.+?(\s|$)')
-NONALPHANUMERIC_re = re.compile(r'[^\w ]')
-TOK_PATH = 'serialized/tokenizer.pickle'
-MODEL_PATH = 'serialized/cpu_friendly_model.h5'
+LINKS_re = re.compile(r"https?://.+?(\s|$)")
+NONALPHANUMERIC_re = re.compile(r"[^\w ]")
+TOK_PATH = "serialized/tokenizer.pickle"
+MODEL_PATH = "serialized/cpu_friendly_model.h5"
 MAXLEN = 200  # more than 99th percentile
 
-LABELS = ['toxicity', 'severe_toxicity', 'identity_attack', 'insult', 'threat']
-
-
-
-print('loading tokenizer..')
-with open(TOK_PATH, 'rb') as handle:
-        tokenizer = pickle.load(handle)
-
-print('loading model..')
-model = load_model(MODEL_PATH)
-
-
-print('done')
+LABELS = ["toxicity", "severe_toxicity", "identity_attack", "insult", "threat"]
 
 
 def clean_text(comment):
-    cleaned = re.sub(LINKS_re, ' ', comment)
-    cleaned = re.sub(NONALPHANUMERIC_re, ' ', cleaned)
-    cleaned = re.sub(r' +', ' ', cleaned)  # collapse consequtive spaces
+    cleaned = re.sub(LINKS_re, " ", comment)
+    cleaned = re.sub(NONALPHANUMERIC_re, " ", cleaned)
+    cleaned = re.sub(r" +", " ", cleaned)  # collapse consequtive spaces
     return cleaned
-
 
 
 def preprocess(text_list):
@@ -54,43 +44,51 @@ def preprocess(text_list):
     return processed_input
 
 
-
-@app.route('/')
+@app.route("/")
 def hello():
     """simple landing Page."""
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/add', methods=['POST'])
+@app.route("/add", methods=["POST"])
 def add():
-    """
-    """
+    """"""
     form_inputs = request.form.to_dict()
-    result = int(form_inputs['a']) + int(form_inputs['b'])
+    result = int(form_inputs["a"]) + int(form_inputs["b"])
 
-    return render_template('index.html', result_text = f'A + B = {result}')
+    return render_template("index.html", result_text=f"A + B = {result}")
 
 
-@app.route('/secret_add', methods=['POST'])
+@app.route("/secret_add", methods=["POST"])
 def secret_add():
     data = request.get_json()
     if data is None:
-        return jsonify({ 'error': 'Missing input' }), 400
-    a = int(data.get('a', 0))
-    b = int(data.get('b', 0))
-    return jsonify({'result':a+b})
+        return jsonify({"error": "Missing input"}), 400
+    a = int(data.get("a", 0))
+    b = int(data.get("b", 0))
+    return jsonify({"result": a + b})
 
 
-
-@app.route('/api/predict', methods=['POST'])
+@app.route("/api/predict", methods=["POST"])
 def api_predict():
     data = request.get_json()
     if data is None:
-        return jsonify({ 'error': 'Missing input' }), 400
-    text = data.get('text', 'N/A')
-    prediction = model.predict(preprocess([text]), batch_size=1, verbose=0)
-    return jsonify({"test":"works"})
-    # return jsonify({label:res for (label, res) in zip(LABELS, prediction)})
+        return jsonify({"error": "Missing input"}), 400
+    text = data.get("text", "N/A")
+    model_input = preprocess([text])
+    with graph.as_default():
+        prediction = model.predict(model_input, batch_size=1, verbose=0).flatten()
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    return jsonify({label: str(res) for (label, res) in zip(LABELS, prediction)})
+
+
+if __name__ == "__main__":
+    print("loading tokenizer..")
+    with open(TOK_PATH, "rb") as handle:
+        tokenizer = pickle.load(handle)
+
+    print("loading model..")
+    model = load_model(MODEL_PATH)
+
+    print("done")
+    app.run(host="127.0.0.1", port=8080, debug=True)
