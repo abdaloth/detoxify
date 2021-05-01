226d4ef18728c91b83c08bd98ee30f4f1ae6ace5
epswartz <eswartzen@gmail.com>
2021-05-01 16:35:46 -0400

    fix locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,19 +1,21 @@
-from locust import FastHttpUser, task
+from locust import HttpUser, task, between
 import json
 
 
-class APIUser(FastHttpUser):
+class APIUser(HttpUser):
     """
     Simulated API user for load testing.
 
     Only has one action, which is calling the single endpoint.
     """
 
+    #wait_time = between(1.5, 2.5)
+
     @task
     def predict(self):
         headers = {'content-type': 'application/json','Accept-Encoding':'gzip'}
         self.client.post(
-            "/predict",
+            "/api/predict",
             data= json.dumps(
                 {
                     "text": "if you don't load test, you're an absolute buffoon!" # model detects toxicity
